3a9f077ffd375168b17160c690847e7c3b613425
Geogre Noise <george.noise.dev+github@gmail.com>
2026-07-18 12:26:30 +0300

    nix

--- lua/plugins/nix-solutions.lua
+++ lua/plugins/nix-solutions.lua
@@ -1,6 +1,14 @@
 -- if true then return {} end
 
 return {
-  { "mason-org/mason.nvim", enabled = false },
-  { "mason-org/mason-lspconfig.nvim", enabled = false },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      PATH = "append", -- use system PATH tools first
+      
+      ui = {
+        -- check_outdated_packages_on_open = false,
+      },
+    },
+  },
 }
