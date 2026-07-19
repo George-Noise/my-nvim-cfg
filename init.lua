d3253866b2ac7f4c6610a2df42ad859ca17cff87
Geogre Noise <george.noise.dev+github@gmail.com>
2026-07-19 21:29:26 +0300

    snippets

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local ls = require("luasnip")
+ls.add_snippets("html", {
+  ls.s("@if", { ls.t("@if(){") }),
+  ls.s("@for", { ls.t("@for (item of items; track $index) {") }),
+});
